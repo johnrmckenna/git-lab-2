66eb4d3d6213a0bf983041e7d7898c09cc991d19
John <jm713015@ohio.edu>
2019-09-03 17:07:24 -0400

    git-lab-program commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: John McKenna
+ *        Date: 9/3/19
+ * Description: Prints "Hello Git!!" *For Lab 2*
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
